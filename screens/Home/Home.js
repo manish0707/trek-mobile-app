23b14d5f32c2ee41d526be5c20b8e15a9ba5d4c6
Manish Sharma <manishsharma959939@gmail.com>
2024-02-03 15:33:03 +0530

    adds setup for icons in android and ios

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function Home() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Icon name="home" size={100} />
       <Text>Home Screen</Text>
     </View>
   );
