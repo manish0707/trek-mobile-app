2cb05bbe86be7d83ef799964434a031eb76c3357
Manish Sharma <manishsharma959939@gmail.com>
2024-02-03 16:10:01 +0530

    adds bottom bar icons

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Colors} from '../styles/Colors';
 import {MaxCreditStack} from './MaxCreditStack';
@@ -13,20 +14,54 @@ const headerStyles = {
   headerTintColor: 'white',
 };
 
+const iconStyles = {
+  borderWidth: 1,
+  borderRadius: 10,
+  paddingVertical: 4,
+  paddingHorizontal: 8,
+  borderColor: 'lightgray',
+};
+
+const TabIcon = ({focused, name}) => {
+  return (
+    <Icon
+      style={focused ? iconStyles : {}}
+      size={28}
+      name={name}
+      color={'black'}
+    />
+  );
+};
+
 export default function Naviation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{header: () => null}}>
+      <Tab.Navigator
+        screenOptions={{header: () => null, tabBarStyle: {height: 80}}}>
         <Tab.Screen
           options={{
             ...headerStyles,
-            headerBackVisible: false,
+            tabBarLabel: () => null,
+            tabBarIcon: values => <TabIcon {...values} name="home" />,
           }}
           name="Home"
           component={Home}
         />
         <Tab.Screen
-          options={headerStyles}
+          options={{
+            ...headerStyles,
+            tabBarLabel: () => null,
+            tabBarIcon: values => <TabIcon {...values} name="wallet" />,
+          }}
+          name="Wallet"
+          component={MaxCreditStack}
+        />
+        <Tab.Screen
+          options={{
+            ...headerStyles,
+            tabBarLabel: () => null,
+            tabBarIcon: values => <TabIcon {...values} name="cards" />,
+          }}
           name="Max Credit"
           component={MaxCreditStack}
         />
