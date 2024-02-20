5bd676d061bcfefa2fe859e1ca85e6394b7450e9
Manish Sharma <manishsharma959939@gmail.com>
2024-02-20 17:46:43 +0530

    adds google login using firebase

---
+++ constants.ts
@@ -0,0 +1,2 @@
+export const webClientId =
+  '171599076764-k9le0ckm8c642fbi26ps1354pjebvgpa.apps.googleusercontent.com';
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Alert, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import {webClientId} from '../../constants';
+import auth from '@react-native-firebase/auth';
+
+GoogleSignin.configure({
+  webClientId: webClientId,
+  offlineAccess: true,
+});
 
 export default function Home() {
+  const signinWithGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+
+      const googleCredentials = await auth.GoogleAuthProvider.credential(
+        userInfo.idToken,
+      );
+
+      await auth().signInWithCredential(googleCredentials);
+
+      Alert.alert('LOGIN SUCCESS', JSON.stringify(userInfo));
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        console.log(error);
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+        console.log(error);
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+        console.log(error);
+      } else {
+        // some other error happened
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Icon name="home" size={100} />
       <Text>Home Screen</Text>
+      <GoogleSigninButton onPress={signinWithGoogle} />
     </View>
   );
 }
