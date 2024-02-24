4125e7d6caf5230e4ccc8543fd7b264cbbdfc512
Manish Sharma <manishsharma959939@gmail.com>
2024-02-24 22:45:18 +0530

    adds loader in expenses list

--- screens/ExpensesList/ExpensesList.js
+++ screens/ExpensesList/ExpensesList.js
@@ -1,5 +1,11 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {View, Text, TouchableOpacity, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {styles} from './ExpensesList.styles';
 import {getMyExpenses} from '../../utils/expenses';
@@ -8,6 +14,7 @@ import {useIsFocused} from '@react-navigation/native';
 
 export default function ExpensesList({navigation}) {
   const [expenses, setExpenses] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const {user} = useContext(AuthContext);
 
   const isfocused = useIsFocused();
@@ -19,12 +26,16 @@ export default function ExpensesList({navigation}) {
   useEffect(() => {
     getMyExpenses(
       user.uid,
-      data => setExpenses(data),
-      error => console.log(error),
+      data => {
+        setExpenses(data);
+        setIsLoading(false);
+      },
+      error => {
+        console.log(error);
+        setIsLoading(false);
+      },
     );
   }, [user.uid, isfocused]);
-
-  console.log(expenses);
 
   return (
     <View style={styles.wrapper}>
@@ -38,36 +49,43 @@ export default function ExpensesList({navigation}) {
         Expenses List
       </Text>
 
-      <FlatList
-        data={expenses}
-        contentContainerStyle={{paddingBottom: 120}}
-        renderItem={({item}) => {
-          return (
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                borderWidth: 1,
-                padding: 10,
-                marginVertical: 10,
-                margin: 10,
-                borderColor: 'lightgray',
-                borderRadius: 4,
-              }}>
-              <View>
-                <Text style={{fontSize: 20, color: 'black'}}>{item.name}</Text>
-                <Text style={{fontSize: 14, color: 'gray', marginTop: 2}}>
-                  {item.cateogry}
+      {isLoading ? (
+        <ActivityIndicator size={40} />
+      ) : (
+        <FlatList
+          data={expenses}
+          contentContainerStyle={{paddingBottom: 120}}
+          renderItem={({item}) => {
+            return (
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  borderWidth: 1,
+                  padding: 10,
+                  marginVertical: 10,
+                  margin: 10,
+                  borderColor: 'lightgray',
+                  borderRadius: 4,
+                }}>
+                <View>
+                  <Text style={{fontSize: 20, color: 'black'}}>
+                    {item.name}
+                  </Text>
+                  <Text style={{fontSize: 14, color: 'gray', marginTop: 2}}>
+                    {item.cateogry}
+                  </Text>
+                </View>
+                <Text
+                  style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+                  ₹{item.amount}
                 </Text>
               </View>
-              <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
-                ₹{item.amount}
-              </Text>
-            </View>
-          );
-        }}
-      />
+            );
+          }}
+        />
+      )}
 
       <TouchableOpacity onPress={handleAdd} style={styles.addExpenseButton}>
         <Icon name="plus" color="white" size={50} />
